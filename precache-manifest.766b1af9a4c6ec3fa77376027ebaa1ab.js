6647437671cae8daa8f32921c1c67183021f135e
Nagesh Kumar T <designer.nagesh@gmail.com>
2020-06-11 11:51:04 +0530

    Updates

---
+++ precache-manifest.766b1af9a4c6ec3fa77376027ebaa1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ade2c5b906051e7231e6dded62b4b6",
+    "url": "/reactjs-shadow-generator/index.html"
+  },
+  {
+    "revision": "12781a3c5d33b5c69c72",
+    "url": "/reactjs-shadow-generator/static/css/2.62c23a74.chunk.css"
+  },
+  {
+    "revision": "98f10fe89d2a3f4cd1b4",
+    "url": "/reactjs-shadow-generator/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "12781a3c5d33b5c69c72",
+    "url": "/reactjs-shadow-generator/static/js/2.482a0dc5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactjs-shadow-generator/static/js/2.482a0dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f10fe89d2a3f4cd1b4",
+    "url": "/reactjs-shadow-generator/static/js/main.e4db41ab.chunk.js"
+  },
+  {
+    "revision": "9d3ad41aeb64e939f0d7",
+    "url": "/reactjs-shadow-generator/static/js/runtime-main.18bc7b21.js"
+  }
+]);
